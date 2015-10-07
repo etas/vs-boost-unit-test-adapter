d501f14e89ac2011604de00ebc66db098f4c5253
Simon Lux <simon.lux@etas.com>
2015-10-07 18:22:52 +0200

    merged #14 from netspiri - Add Boost License to Source Files

--- BoostTestAdapterNunit/BoostTestRunnerCommandLineArgsTest.cs
+++ BoostTestAdapterNunit/BoostTestRunnerCommandLineArgsTest.cs
@@ -1,4 +1,9 @@
-﻿using BoostTestAdapter.Boost.Runner;
+﻿// (C) Copyright ETAS 2015.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+using BoostTestAdapter.Boost.Runner;
 using NUnit.Framework;
 
 namespace BoostTestAdapterNunit
--- VisualStudio2013Adapter/VisualStudio2013.cs
+++ VisualStudio2013Adapter/VisualStudio2013.cs
@@ -1,4 +1,9 @@
-﻿namespace VisualStudio2013Adapter
+﻿// (C) Copyright ETAS 2015.
+// Distributed under the Boost Software License, Version 1.0.
+// (See accompanying file LICENSE_1_0.txt or copy at
+// http://www.boost.org/LICENSE_1_0.txt)
+
+namespace VisualStudio2013Adapter
 {
     /// <summary>
     /// Adapter for a Visual Studio 2013 running instance.
